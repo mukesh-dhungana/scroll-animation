143b1d78e7c2a461b72ed8a713b78450bb820b75
Mukesh Dhungana <mukesh@rigonepal.com>
2019-08-25 16:18:46 +0545

    added pages

---
+++ assets/js/animation.js
@@ -0,0 +1,432 @@
+var isScrolling = false;
+window.onscroll = () => myFunction();
+function myFunction() {
+  const animationSectionLen = document.getElementsByClassName(
+    "animation-sections"
+  ).length;
+  const windowHeight = window.innerHeight;
+  for (a = 0; a < animationSectionLen; a++) {
+    if (
+      document
+        .getElementsByClassName("animation-sections")
+        [a].getBoundingClientRect().top +
+        document.getElementsByClassName("animation-sections")[a].offsetHeight /
+          2 -
+        windowHeight >
+        -1000 &&
+      document
+        .getElementsByClassName("animation-sections")
+        [a].getBoundingClientRect().top +
+        document.getElementsByClassName("animation-sections")[a].offsetHeight /
+          2 -
+        windowHeight <
+        150
+    ) {
+      document.getElementsByClassName("video-play")[a].play();
+    } else {
+      document.getElementsByClassName("video-play")[a].pause();
+    }
+    if (
+      document
+        .getElementsByClassName("animation-sections")
+        [a].getBoundingClientRect().top < 2
+    ) {
+      //document.getElementById("videoDiv").style.width = "90%";
+      document.getElementsByClassName("animation-sidebar-right")[
+        a
+      ].style.transform = "scale(0, 1)";
+      document.getElementsByClassName("animation-sidebar-left")[
+        a
+      ].style.transform = "scale(0, 1)";
+      document.getElementsByClassName("animation-sidebar-top")[
+        a
+      ].style.transform = "scale(1, 0.57)";
+      document.getElementsByClassName("animation-sidebar-bottom")[
+        a
+      ].style.transform = "scale(1, 0)";
+      for (i = a * 3; i < a * 3 + 3; i++) {
+        if (window.innerWidth < 770) {
+          document.getElementsByClassName("animation-element")[
+            i
+          ].style.transform = "translate(45px, -50px)";
+        } else {
+          document.getElementsByClassName("animation-element")[
+            i
+          ].style.transform = "translate(110px, -50px)";
+        }
+        document.getElementsByClassName("animation-element")[
+          i
+        ].style.animation = "5s ease 0s normal forwards 1 fadein";
+      }
+
+      //document.getElementsByClassName("video-play")[a].play();
+    }
+    if (
+      document
+        .getElementsByClassName("animation-sections")
+        [a].getBoundingClientRect().top < 200 &&
+      document
+        .getElementsByClassName("animation-sections")
+        [a].getBoundingClientRect().top > 2
+    ) {
+      //document.getElementById("videoDiv").style.width = "90%";
+      document.getElementsByClassName("animation-sidebar-right")[
+        a
+      ].style.transform = "scale(0.596591, 1)";
+      document.getElementsByClassName("animation-sidebar-left")[
+        a
+      ].style.transform = "scale(0.596591, 1)";
+      document.getElementsByClassName("animation-sidebar-top")[
+        a
+      ].style.transform = "scale(1, 0.826534)";
+      document.getElementsByClassName("animation-sidebar-bottom")[
+        a
+      ].style.transform = "scale(1, 0.596591)";
+      for (i = a * 3; i < a * 3 + 3; i++) {
+        if (window.innerWidth < 770) {
+          document.getElementsByClassName("animation-element")[
+            i
+          ].style.transform = "translate(45px, -50px)";
+        } else {
+          document.getElementsByClassName("animation-element")[
+            i
+          ].style.transform = "translate(110px, -50px)";
+        }
+        document.getElementsByClassName("animation-element")[
+          i
+        ].style.animation = "5s ease 0s normal forwards 1 fadein";
+      }
+
+      //document.getElementsByClassName("video-play")[a].play();
+    }
+
+    if (
+      document
+        .getElementsByClassName("animation-sections")
+        [a].getBoundingClientRect().top > 200
+    ) {
+      document.getElementsByClassName("animation-sidebar-right")[
+        a
+      ].style.transform = "scale(1, 1)";
+      document.getElementsByClassName("animation-sidebar-left")[
+        a
+      ].style.transform = "scale(1, 1)";
+      document.getElementsByClassName("animation-sidebar-top")[
+        a
+      ].style.transform = "scale(1, 1)";
+      document.getElementsByClassName("animation-sidebar-bottom")[
+        a
+      ].style.transform = "scale(1, 1)";
+      for (i = a * 3; i < a * 3 + 3; i++) {
+        if (window.innerWidth < 770) {
+          document.getElementsByClassName("animation-element")[
+            i
+          ].style.transform = "translate(45px, 100px)";
+        } else {
+          document.getElementsByClassName("animation-element")[
+            i
+          ].style.transform = "translate(110px, 120px)";
+        }
+        document.getElementsByClassName("animation-element")[
+          i
+        ].style.animation = "0.5s ease 0s normal forwards 1 fadeout";
+      }
+      //document.getElementsByClassName("video-play")[a].pause();
+    }
+  }
+  var transitionalSectionLen = document.getElementsByClassName(
+    "transitional-section"
+  ).length;
+  for (item = 0; item < transitionalSectionLen; item++) {
+    var transitionalSection = document.getElementsByClassName(
+      "transitional-section"
+    )[item];
+    if (
+      window.innerHeight -
+        document
+          .getElementsByClassName("transitional-section")
+          [item].getBoundingClientRect().top <
+      20
+    ) {
+      transformTransitionalEl(120, transitionalSection.parentElement);
+    }
+    if (
+      window.innerHeight -
+        document
+          .getElementsByClassName("transitional-section")
+          [item].getBoundingClientRect().top >
+        20 &&
+      window.innerHeight -
+        document
+          .getElementsByClassName("transitional-section")
+          [item].getBoundingClientRect().top <=
+        100
+    ) {
+      transformTransitionalEl(45, transitionalSection.parentElement);
+    }
+    if (
+      window.innerHeight -
+        document
+          .getElementsByClassName("transitional-section")
+          [item].getBoundingClientRect().top >=
+        200 &&
+      window.innerHeight -
+        document
+          .getElementsByClassName("transitional-section")
+          [item].getBoundingClientRect().top <
+        window.innerHeight
+    ) {
+      transformTransitionalEl(0, transitionalSection.parentElement);
+    }
+    let transitionLowerHeight = 500,
+      transitionUpperHeight = 600;
+    if (window.innerHeight < 700) {
+      transitionLowerHeight = 300;
+      transitionUpperHeight = 500;
+    } else if (window.innerHeight > 700 && window.innerHeight < 900) {
+      transitionLowerHeight = 600;
+      transitionUpperHeight = 700;
+    } else if (window.innerHeight > 900 && window.innerHeight < 1100) {
+      transitionLowerHeight = 700;
+      transitionUpperHeight = 900;
+    } else if (window.innerHeight > 1100 && window.innerHeight < 1300) {
+      transitionLowerHeight = 1050;
+      transitionUpperHeight = 1100;
+    }
+
+    if (
+      window.innerHeight -
+        document
+          .getElementsByClassName("transitional-section")
+          [item].getBoundingClientRect().bottom >=
+        transitionLowerHeight &&
+      window.innerHeight -
+        document
+          .getElementsByClassName("transitional-section")
+          [item].getBoundingClientRect().bottom <=
+        transitionUpperHeight
+    ) {
+      transformTransitionalEl(-60, transitionalSection.parentElement);
+    }
+    if (
+      window.innerHeight -
+        document
+          .getElementsByClassName("transitional-section")
+          [item].getBoundingClientRect().bottom >=
+      transitionUpperHeight
+    ) {
+      transformTransitionalEl(-80, transitionalSection.parentElement);
+    }
+    // if (isScrolledIntoView(transitionalSection)) {
+    //   transformTransitionalEl(true, transitionalSection.parentElement);
+    // } else {
+    //   transformTransitionalEl(false, transitionalSection.parentElement);
+    // }
+  }
+
+  if (
+    document.getElementById("homeSection").getBoundingClientRect().bottom <
+      500 &&
+    document.getElementById("homeSection").getBoundingClientRect().bottom > 200
+  ) {
+    var homeAnimationLen = document.getElementsByClassName("home-animation")
+      .length;
+    for (i = 0; i < homeAnimationLen; i++) {
+      let translate = "0";
+      const windowWidth = window.innerWidth;
+      switch (true) {
+        case windowWidth > 767 && windowWidth <= 992:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(45px, -270px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(47px, -90px)";
+          }
+
+          break;
+        case windowWidth <= 568:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(45px, -140px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(47px, -70px)";
+          }
+          break;
+        default:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(145px,-500px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = " translate(150px, -300px)";
+          }
+          let fade = "fadeout";
+          fade = isScrolling ? "fadeoutFast" : "fadeout";
+          document.getElementsByClassName("home-animation")[i].style.animation =
+            "2s ease 0s normal forwards 1 " + fade;
+      }
+    }
+  } else {
+    var homeAnimationLen = document.getElementsByClassName("home-animation")
+      .length;
+    for (i = 0; i < homeAnimationLen; i++) {
+      let translate = "0";
+      const windowWidth = window.innerWidth;
+      switch (true) {
+        case windowWidth > 767 && windowWidth <= 992:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(45px, -370px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(47px, -190px)";
+          }
+          break;
+        case windowWidth <= 568:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(45px, -180px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(47px, -100px)";
+          }
+          break;
+        default:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(145px,-400px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = " translate(150px, -205px)";
+          }
+      }
+
+      document.getElementsByClassName("home-animation")[i].style.animation =
+        "5s ease 0s normal forwards 1 fadein";
+    }
+  }
+}
+const videoCount = document.getElementsByClassName("video-play").length;
+for (i = 0; i < videoCount; i++) {
+  const video = document.getElementsByClassName("video-play")[i];
+  video.addEventListener("ended", () => {
+    video.currentTime = 0.05;
+    video.play();
+  });
+}
+const videoHome = document.getElementById("video-home");
+videoHome.addEventListener("ended", () => {
+  videoHome.currentTime = 0.05;
+  videoHome.play();
+});
+
+var isScrolledIntoView = elem => {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+
+  var elemTop = elem.offsetTop;
+  var elemBottom = elemTop + elem.offsetHeight;
+
+  return elemBottom <= docViewBottom + 200 && elemTop >= docViewTop;
+};
+
+var transformTransitionalEl = (transtionSize, transitionalSection) => {
+  var transitionalDiv = transitionalSection.getElementsByClassName(
+    "transitional-content-div"
+  );
+  for (tLen = 0; tLen < transitionalDiv.length; tLen++) {
+    var transitionalContentLen = transitionalDiv[tLen].getElementsByClassName(
+      "transitional-content"
+    ).length;
+    for (i = 0; i < transitionalContentLen; i++) {
+      transitionalDiv[tLen].getElementsByClassName("transitional-content")[
+        i
+      ].style.transform = "translate(0," + transtionSize + "px)";
+      transitionalDiv[tLen].getElementsByClassName("transitional-content")[
+        i
+      ].style.animation = "5s ease 0s normal forwards 1 fadein";
+    }
+  }
+};
+
+//home-section-onload-animation
+$(document).ready(() => {
+  setTimeout(() => {
+    var homeAnimationLen = document.getElementsByClassName("home-animation")
+      .length;
+    for (i = 0; i < homeAnimationLen; i++) {
+      let translate = "0";
+      const windowWidth = window.innerWidth;
+      switch (true) {
+        case windowWidth > 767 && windowWidth <= 992:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(45px, -270px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(47px, -90px)";
+          }
+          break;
+        case windowWidth <= 568:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(45px, -140px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(47px, -70px)";
+          }
+          break;
+        default:
+          if (i == 0 || i == 1) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = "translate(145px,-400px)";
+          }
+
+          if (i == 2) {
+            document.getElementsByClassName("home-animation")[
+              i
+            ].style.transform = " translate(150px, -205px)";
+          }
+      }
+
+      document.getElementsByClassName("home-animation")[i].style.animation =
+        "5s ease 0s normal forwards 1 fadein";
+    }
+  }, 1000);
+});
+//......//
---
+++ assets/js/main.js
@@ -0,0 +1,87 @@
+(function($) {
+  $(".hero-area-slider").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: true,
+    animateIn: "fadeIn",
+    animateOut: "fadeOut",
+    mouseDrag: false
+  });
+
+  $(".testimonial-slider").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: true
+  });
+
+  // init Isotope
+  var $grid = $(".portfolio-container").isotope({
+    masonry: {
+      gutter: 10
+    }
+  });
+  // filter items on button click
+  $(".portfolio-filter").on("click", "li", function() {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({
+      filter: filterValue
+    });
+  });
+
+  //On click change menu color
+  $(".navbar-nav").on("click", "li", function() {
+    $(".navbar-nav li a").removeClass("active");
+    $(this)
+      .children("a")
+      .addClass("active");
+  });
+
+  $(".scrl-down").on("click", function(e) {
+    isScrolling = $(this).attr("href") === "#aboutSection" ? false : true;
+    e.preventDefault();
+    let speed = 1000;
+    if (
+      $(this).attr("href") == "#portfolioSection" ||
+      $(".scrl-down.active").attr("href") == "#portfolioSection"
+    ) {
+      speed = 3000;
+    }
+    $(".animation-content").map((i, x) => $(x).addClass("animation-inactive"));
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top
+      },
+      {
+        duration: speed,
+        easing: "linear",
+        complete: function() {
+          $(".animation-content").map((i, x) =>
+            $(x).removeClass("animation-inactive")
+          );
+          isScrolling = false;
+        }
+      }
+    );
+  });
+  //On scroll menu fixed to top
+  // var menu = $("nav");
+  // $(window).scroll(function () {
+  //more then or equals to
+  // if ($(window).scrollTop() < 800) {
+  // 	menu.removeClass('fixed-top animated slideInDown');
+
+  // 	//less then 100px from top
+  // } else {
+  // 	menu.addClass('fixed-top animated slideInDown');
+
+  // }
+  //});
+
+  //	$('.scrollbar-body').mCustomScrollbar({
+  //		theme: "dark",
+  //		mouseWheel: {
+  //			scrollAmount: 150
+  //		},
+  //		setHeight: '100%',
+  //	});
+})(jQuery);
